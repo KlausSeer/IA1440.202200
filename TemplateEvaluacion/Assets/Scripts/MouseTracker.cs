f6b1e1e078bcea2c00bfaa89b40600347a74887b
KlausSeer <33689728+KlausSeer@users.noreply.github.com>
2022-01-18 19:36:47 -0500

    FirstCommit

---
+++ TemplateEvaluacion/Assets/Scripts/MouseTracker.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseTracker : MonoBehaviour
+{
+    private Vector2 mousePosition;
+
+    public Vector2 MousePosition { get { return mousePosition; } }
+
+
+    void Start()
+    {
+        mousePosition = new Vector2();
+    }
+
+
+    void Update()
+    {
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+}
